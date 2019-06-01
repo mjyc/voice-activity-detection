2b0630c0d1c9ee03627173b11c6d238f197b5fd6
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-06-01 07:38:20 -0700

    FORMATTING

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-var vad = require('../index.js');
+var vad = require("../index.js");
 var audioContext;
 
-var valueContainer = document.createElement('div');
+var valueContainer = document.createElement("div");
 document.body.appendChild(valueContainer);
 
-var stateContainer = document.createElement('div');
+var stateContainer = document.createElement("div");
 document.body.appendChild(stateContainer);
 
 requestMic();
@@ -14,34 +14,44 @@ function requestMic() {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     audioContext = new AudioContext();
 
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    navigator.getUserMedia({audio: true}, startUserMedia, handleMicConnectError);
+    navigator.getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia;
+    navigator.getUserMedia(
+      { audio: true },
+      startUserMedia,
+      handleMicConnectError
+    );
   } catch (e) {
     handleUserMediaError();
   }
 }
 
 function handleUserMediaError() {
-  console.warn('Mic input is not supported by the browser.');
+  console.warn("Mic input is not supported by the browser.");
 }
 
 function handleMicConnectError() {
-  console.warn('Could not connect microphone. Possible rejected by the user or is blocked by the browser.');
+  console.warn(
+    "Could not connect microphone. Possible rejected by the user or is blocked by the browser."
+  );
 }
 
 function startUserMedia(stream) {
   var options = {
     onVoiceStart: function() {
-      console.log('voice start');
-      stateContainer.innerHTML = 'Voice state: <strong>active</strong>';
+      console.log("voice start");
+      stateContainer.innerHTML = "Voice state: <strong>active</strong>";
     },
     onVoiceStop: function() {
-      console.log('voice stop');
-      stateContainer.innerHTML = 'Voice state: <strong>inactive</strong>';
+      console.log("voice stop");
+      stateContainer.innerHTML = "Voice state: <strong>inactive</strong>";
     },
     onUpdate: function(val) {
-      //console.log('curr val:', val);
-      valueContainer.innerHTML = 'Current voice activity value: <strong>' + val + '</strong>';
+      valueContainer.innerHTML =
+        "Current voice activity value: <strong>" + val + "</strong>";
     }
   };
   vad(audioContext, stream, options);
