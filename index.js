19d1460ea74858c38fcff189426bf612de4240c3
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-05-31 22:19:04 -0700

    FORMATTING

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-'use strict';
-var analyserFrequency = require('analyser-frequency-average');
+"use strict";
+var analyserFrequency = require("analyser-frequency-average");
 
 module.exports = function(audioContext, stream, opts) {
-
   opts = opts || {};
 
   var defaults = {
     fftSize: 1024,
     bufferLen: 1024,
     smoothingTimeConstant: 0.2,
-    minCaptureFreq: 85,         // in Hz
-    maxCaptureFreq: 255,        // in Hz
+    minCaptureFreq: 85, // in Hz
+    maxCaptureFreq: 255, // in Hz
     noiseCaptureDuration: 1000, // in ms
-    minNoiseLevel: 0.3,         // from 0 to 1
-    maxNoiseLevel: 0.7,         // from 0 to 1
+    minNoiseLevel: 0.3, // from 0 to 1
+    maxNoiseLevel: 0.7, // from 0 to 1
     avgNoiseMultiplier: 1.2,
-    onVoiceStart: function() {
-    },
-    onVoiceStop: function() {
-    },
-    onUpdate: function(val) {
-    }
+    onVoiceStart: function() {},
+    onVoiceStop: function() {},
+    onUpdate: function(val) {}
   };
 
   var options = {};
@@ -46,31 +42,39 @@ module.exports = function(audioContext, stream, opts) {
   analyser.smoothingTimeConstant = options.smoothingTimeConstant;
   analyser.fftSize = options.fftSize;
 
-  var scriptProcessorNode = audioContext.createScriptProcessor(options.bufferLen, 1, 1);
+  var scriptProcessorNode = audioContext.createScriptProcessor(
+    options.bufferLen,
+    1,
+    1
+  );
   connect();
   scriptProcessorNode.onaudioprocess = monitor;
 
   if (isNoiseCapturing) {
-    //console.log('VAD: start noise capturing');
     captureTimeout = setTimeout(init, options.noiseCaptureDuration);
   }
 
   function init() {
-    //console.log('VAD: stop noise capturing');
     isNoiseCapturing = false;
 
-    envFreqRange = envFreqRange.filter(function(val) {
-      return val;
-    }).sort();
-    var averageEnvFreq = envFreqRange.length ? envFreqRange.reduce(function (p, c) { return Math.min(p, c) }, 1) : (options.minNoiseLevel || 0.1);
+    envFreqRange = envFreqRange
+      .filter(function(val) {
+        return val;
+      })
+      .sort();
+    var averageEnvFreq = envFreqRange.length
+      ? envFreqRange.reduce(function(p, c) {
+          return Math.min(p, c);
+        }, 1)
+      : options.minNoiseLevel || 0.1;
 
     baseLevel = averageEnvFreq * options.avgNoiseMultiplier;
-    if (options.minNoiseLevel && baseLevel < options.minNoiseLevel) baseLevel = options.minNoiseLevel;
-    if (options.maxNoiseLevel && baseLevel > options.maxNoiseLevel) baseLevel = options.maxNoiseLevel;
+    if (options.minNoiseLevel && baseLevel < options.minNoiseLevel)
+      baseLevel = options.minNoiseLevel;
+    if (options.maxNoiseLevel && baseLevel > options.maxNoiseLevel)
+      baseLevel = options.maxNoiseLevel;
 
     voiceScale = 1 - baseLevel;
-
-    //console.log('VAD: base level:', baseLevel);
   }
 
   function connect() {
@@ -95,7 +99,12 @@ module.exports = function(audioContext, stream, opts) {
     var frequencies = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(frequencies);
 
-    var average = analyserFrequency(analyser, frequencies, options.minCaptureFreq, options.maxCaptureFreq);
+    var average = analyserFrequency(
+      analyser,
+      frequencies,
+      options.minCaptureFreq,
+      options.maxCaptureFreq
+    );
     if (isNoiseCapturing) {
       envFreqRange.push(average);
       return;
@@ -124,5 +133,5 @@ module.exports = function(audioContext, stream, opts) {
     options.onVoiceStop();
   }
 
-  return {connect: connect, disconnect: disconnect, destroy: destroy};
+  return { connect: connect, disconnect: disconnect, destroy: destroy };
 };
